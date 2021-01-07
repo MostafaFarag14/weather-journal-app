1712d4e57ced5825ab849d5f72340b330a3e16b0
MostafaFarag14 <mostafafarag140@gmail.com>
2021-01-07 08:59:35 +0200

    implement all features

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Server routes
+app.get('/recieveData', (req, res) => {
+  res.send(projectData)
+})
+
+app.post('/sendData', (req, res) => {
+  projectData = req.body
+  res.send(projectData)
+})
 
 // Setup Server
+const port = 8000
+const server = app.listen( port , () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,59 @@
+
 /* Global Variables */
+const generateButton = document.querySelector('#generate')
+const dateElement = document.querySelector('#date')
+const tempElement = document.querySelector('#temp')
+const contentElement = document.querySelector('#content')
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'b571894544916e3dbe6d8e51836b07dc'
+const baseURL = `http://api.openweathermap.org/data/2.5/weather?units=metric&`
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
+/* Function to GET Web API Data*/
+const getWeatherData = () => {
+  // Get input data from HTML
+  const zipCode = document.querySelector('#zip').value
+  const feelings = document.querySelector('#feelings').value
+  
+  fetch(`${baseURL}zip=${zipCode}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(jsonRes => postData('/sendData', {
+      date: newDate,
+      temp: jsonRes.main.temp,
+      content: feelings
+    }))
+    .catch(error => console.log(error))
+}
+// Event listener to add function to existing HTML DOM element
+generateButton.addEventListener('click', getWeatherData)
+
+/* Function to update UI*/
+const updateUI = async () => {
+  const { date, temp, content } = await getData()
+  dateElement.innerHTML = date
+  tempElement.innerHTML = `${temp} Celsius`
+  contentElement.innerHTML = content
+}
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(updateUI)
+}
+
+/* Function to GET Project Data */
+const getData = async () => {
+  const response = await fetch('/recieveData')
+  const data = await response.json()
+  return data
+}
